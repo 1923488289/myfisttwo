97db7c7c85b829a53deea78602042094a53d8b55
1923488289 <1923488289@qq.com>
2019-04-19 22:37:53 +0800

    版本3

--- test1/testbook/urls.py
+++ test1/testbook/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import url
 from testbook import views
 
 urlpatterns = [
-    url(r'^$',views.index),
-    url(r'^uppic$',views.uppic),
-    url(r'^ccpic$',views.ccpic),
-    url(r'^select$',views.select),
+
+    url(r'^$',views.index),#主页
+    url(r'^uppic$',views.uppic),#上传图书信息
+    url(r'^ccpic$',views.ccpic),#信息保存
+    url(r'^select$',views.select),#查询书籍
+    url(r'^heromess(\d+)$',views.heromess),#查询信息
+    url(r'^upcreate$',views.upcreate),#上传信息
+    url(r'^heromessage(\d+)$',views.heromessage),#保存信息
 
 ]
--- test1/testbook/views.py
+++ test1/testbook/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from test1 import settings
 from testbook.models import HeroInfo, BookInfo
 
 
 # /
 def index(request):
-    return render(request,'testbook/index.html')
+    return render(request, 'testbook/index.html')
 
 
 # /uppic
@@ -33,9 +32,54 @@ def ccpic(request):
         bcomment=bcomment,
         bimg='testbook/%s' % pic.name
     )
-    return HttpResponse('恭喜你上传成功')
+    # b=BookInfo.objects.filter(btitle=sm)
+    return render(request, 'testbook/uphero.html')
+
+
+# /select
 def select(request):
-    b=BookInfo.objects.all()
-    return render(request,'testbook/select.html',{'b':b})
+    b = BookInfo.objects.all()
+    return render(request, 'testbook/select.html', {'b': b})
 
 
+# heromess
+def heromess(request, i):
+    b = BookInfo.objects.get(id=int(i))
+    heros = b.heroinfo_set.all()
+    return render(request, 'testbook/selecthero.html', {'heros': heros})
+
+
+# upcreate
+def upcreate(request):
+    hname = request.POST.get('hname')
+    hgender = request.POST.get('hgender')
+    hcomment = request.POST.get('hcomment')
+    hbook = request.POST.get('hbook')
+    pic = request.FILES.get('pichero')
+    pic_url = '%s/testhero/%s' % (settings.MEDIR_ROOL, pic.name)
+    with open(pic_url, 'wb') as file:
+        for file_1 in pic.chunks():
+            file.write(file_1)
+    if BookInfo.objects.filter(btitle=hbook):
+        b = BookInfo.objects.get(btitle=hbook)
+        # print(a)
+        # b=BookInfo.objects.get(id=a.id)
+        print(b)
+        HeroInfo.objects.create(
+            hname=hname,
+            hgender=int(hgender),
+            hcomment=hcomment,
+            hbook=b,
+            himg='testhero/%s' % pic.name
+        )
+
+        return HttpResponse('信息上传完成')
+    else:
+        return render(request, 'testbook/err.html')
+
+
+# /heromessage
+def heromessage(request, i):
+    b = BookInfo.objects.get(id=int(i))
+    heros = b.heroinfo_set.all()
+    return render(request, 'testbook/heros.html', {'heros': heros})
